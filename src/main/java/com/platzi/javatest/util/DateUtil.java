9f29389678e380796870269f5f8bbf23f196c01b
sirlocust <jua56yu@hotmail.com>
2020-10-20 16:11:45 -0500

    change return statement isLeapYear

--- src/main/java/com/platzi/javatest/util/DateUtil.java
+++ src/main/java/com/platzi/javatest/util/DateUtil.java
@@ -3,11 +3,8 @@ package com.platzi.javatest.util;
 public class DateUtil {
 
     public static boolean isLeapYear(int year) {
-        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
-            return true;
-        }
 
-        return false;
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
     }
 
 }
